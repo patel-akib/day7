d38afd12b26d9eae366fd4e6adb1c58e263729af
aakib patel <akibgordhan@gmail.com>
2024-10-07 22:00:26 -0700

    first commit

---
+++ aakib.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main(){
+	int age=20;
+	printf("aakib%d",age);
+	 age=34;
+	printf("\n%d jisan",age);
+	age=28;
+	printf("\nmalhar%d",age);
+	age=45;
+	printf("\nyahya%d",age);
+	age=56;
+		printf("\nnoman%d",age);
+}
+
+
+
+
+
+
+	
+
+
